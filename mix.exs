543ce10d7d76bbe32bd30bde15bf4c6a8518fca7
Hilder Santos <hilder@hildersantos.com>
2020-12-01 22:23:15 -0300

    Add Norm dependency
    This dependency is needed for checking input contracts. As we're going
    to deal with external files - e.g. the json database - we need to make
    sure everything is as expected by the server.

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule TheRush.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:norm, "~> 0.12"},
     ]
   end
 
