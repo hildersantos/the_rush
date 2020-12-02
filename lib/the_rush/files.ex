42ff5d4339882bc0eee67fafe3261a7295e31511
Hilder Santos <hilder@hildersantos.com>
2020-12-02 01:26:19 -0300

    Add Filters module
    This module reads the contents of a valid JSON file and returns its
    contents in a format Elixir can process.

---
+++ lib/the_rush/files.ex
@@ -0,0 +1,21 @@
+defmodule TheRush.Files do
+  @moduledoc """
+  Reads file and returns JSON struct
+  """
+
+  @callback fetch_json(String.t()) :: [map()]
+
+  @doc """
+  Handles JSON files
+  """
+  def fetch_json(filename) do
+    path = 
+      "#{File.cwd!()}/priv/support_files/#{filename}"
+
+    path
+    |> File.stream!()
+    |> Stream.map(&String.trim/1)
+    |> Enum.join("")
+    |> Jason.decode!()
+  end
+end
