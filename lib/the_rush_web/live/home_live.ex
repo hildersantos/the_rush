ffa602bbe1cac3b7d044550dc338378c1544986e
Hilder Santos <hilder@hildersantos.com>
2020-12-09 01:26:17 -0300

    Fetch website table rows from database
    We're now starting to fetch players directly from database, instead of
    using placeholder info.

--- lib/the_rush_web/live/home_live.ex
+++ lib/the_rush_web/live/home_live.ex
@@ -4,14 +4,45 @@ defmodule TheRushWeb.HomeLive do
   """
   use TheRushWeb, :live_view
 
+  alias TheRush.Players
+
+  @per_page 20
+
   @impl true
   def mount(_params, _session, socket) do
+    # Default values
+    pagination_opts = %{page: 1, per_page: @per_page}
+    sort_opts = %{sort_by: :name, sort_order: :asc}
+
     assigns = [
-      records: [],
-      search_query: "",
-      sort_by: nil
+      options: Map.merge(pagination_opts, sort_opts),
+      players: Players.all(
+        pagination: pagination_opts,
+        sort: sort_opts
+      )
     ]
 
-    {:ok, assign(socket, assigns)}
+    {:ok, assign(socket, assigns), temporary_assigns: [players: []]}
+  end
+
+  @impl true
+  def handle_params(params, _uri, socket) do
+    page = String.to_integer(params["page"] || "1")
+    per_page = String.to_integer(params["per_page"] || "#{@per_page}")
+    sort_by = (params["sort_by"] || "name") |> String.to_atom()
+    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+
+    pagination_opts = %{page: page, per_page: per_page}
+    sort_opts = %{sort_by: sort_by, sort_order: sort_order}
+
+    assigns = [
+      options: Map.merge(pagination_opts, sort_opts),
+      players: Players.all(
+        pagination: pagination_opts,
+        sort: sort_opts
+      )
+    ]
+
+    {:noreply, assign(socket, assigns)}
   end
 end
